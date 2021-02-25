bbd80d03bc72a698638eec27284b11028018553c
sevinm <79662332+sevinm@users.noreply.github.com>
2021-02-26 00:22:20 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class armstrong{
+    
+public static void main (String[] args) {
+    int c=0,a,temp;
+    int n = 153;
+    temp = n;
+    while(n>0){
+        a=n%10;
+        n=n/10;
+        c=c+(a*a*a);
+    }
+        if(temp==c){
+            System.out.println("armstrong");
+        }
+        else{
+            System.out.println("not armstrong");
+        }
+    
+}
+}
